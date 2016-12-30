a1a38390f96db254af8d4b1088279f9b4bebcec6
Maor Eini <maor.eini@gmail.com>
2016-12-31 00:14:18 +0200

    ME: Implemented Task 2 requirements
    note that properties are not being injected from the messages to the templates

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var express = require('express');
 var messageManager = require('./messageManager.js');
 
 var app = express();
+
+//app.set("views", "./views/");
+//app.set("views/templates/", "./views/templates");
 app.set('view engine', 'ejs');
+
+app.use(express.static("./views/templates/"));
 
 app.get('/', function (req, res) {
     res.render('index', {title:'Hello', user:'Lucas'});
@@ -14,17 +19,31 @@ app.get('/', function (req, res) {
 
 app.get('/screen', function (req, res) {
     var msgs = messageManager.GetMessagesByScreenId(req.query.id);
-    var timeToFinish = 0;
+    var templates = msgs.map(function(obj) { return obj.template; });
+    var uniqueTemplates = templates.filter(function(v,i) { return templates.indexOf(v) == i; });
 
-    msgs.map(function (msg) {
-        setTimeout(function () {
-            timeToFinish = timeToFinish + 2000;
 
-            res.render(msg.template, msg); //TODO: idea - slice this message and pass the rest of the array to the template's javascript that will create ajax call for the next message somehow.
-        },timeToFinish);
-    });
+    //
+    // var flags = [], output = [], l = msgs.length, i;
+    // for( i=0; i<l; i++) {
+    //     if( flags[msgs[i].template]) continue;
+    //     flags[msgs[i].template] = true;
+    //     output.push(msgs[i].template);
+    // }
 
-    console.log(timeToFinish);
+    res.render('index', {title:'Hello', user:'Lucas', msgs: msgs });
+
+    // var timeToFinish = 0;
+
+    // msgs.map(function (msg) {
+    //     setTimeout(function () {
+    //         timeToFinish = timeToFinish + 2000;
+    //
+    //         res.render(msg.template, msg); //TODO: idea - slice this message and pass the rest of the array to the template's javascript that will create ajax call for the next message somehow.
+    //     },timeToFinish);
+    // });
+
+    console.log('was here');
 
 });
 
---
+++ data/messages.js
@@ -0,0 +1,166 @@
+var msgs = [
+    {
+        "name": "message1",
+        "text": [
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla"
+        ],
+        "images": [
+            "img1",
+            "img2"
+        ],
+        "template": "templateA.html",
+        "length": 10,
+        "timeFrame": [
+            {
+                "fromDate": "01/01/2016",
+                "toDate": "12/31/2016",
+                "startTime": "06:00",
+                "endTime": "12:00",
+                "weekDays": [
+                    2
+                ]
+            },
+            {
+                "fromDate": "01/01/2016",
+                "toDate": "12/31/2016",
+                "startTime": "13:00",
+                "endTime": "20:00",
+                "weekDays": [
+                    4
+                ]
+            }
+        ],
+        "stations": [
+            1,
+            2
+        ]
+    },
+    {
+        "name": "message2",
+        "text": [
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla"
+        ],
+        "images": [
+            "img1"
+        ],
+        "template": "templateB.html",
+        "length": 10,
+        "timeFrame": [
+            {
+                "fromDate": "03/01/2016",
+                "toDate": "12/31/2016",
+                "startTime": "10:00",
+                "endTime": "23:00",
+                "weekDays": [
+                    3,
+                    4,
+                    5
+                ]
+            }
+        ],
+        "stations": [
+            1,
+            3
+        ]
+    },
+    {
+        "name": "message3",
+        "text": [
+            'bla bla bla'
+        ],
+        "images": [],
+        "template": "templateC.html",
+        "length": 10,
+        "timeFrame": [
+            {
+                "fromDate": "05/01/2016",
+                "toDate": "06/15/2017",
+                "startTime": "08:00",
+                "endTime": "23:00",
+                "weekDays": [
+                    0,
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6
+                ]
+            }
+        ],
+        "stations": [
+            2,
+            3
+        ]
+    },
+    {
+        "name": "message4",
+        "text": [
+            "bla bla bla",
+            "bla bla bla"
+        ],
+        "images": [],
+        "template": "templateA.html",
+        "length": 10,
+        "timeFrame": [
+            {
+                "fromDate": "03/29/2016",
+                "toDate": "04/15/2016",
+                "startTime": "15:00",
+                "endTime": "19:00",
+                "weekDays": [
+                    2
+                ]
+            }
+        ],
+        "stations": [
+            1
+        ]
+    },
+    {
+        "name": "message5",
+        "text": [
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla",
+            "bla bla bla"
+        ],
+        "images": [
+            "img1",
+            "img2"
+        ],
+        "template": "templateB.html",
+        "length": 10,
+        "timeFrame": [
+            {
+                "fromDate": "04/01/2016",
+                "toDate": "04/30/2016",
+                "startTime": "01:00",
+                "endTime": "23:00",
+                "weekDays": [
+                    3
+                ]
+            }
+        ],
+        "stations": [
+            1
+        ]
+    }
+];
+
+module.exports = msgs;
